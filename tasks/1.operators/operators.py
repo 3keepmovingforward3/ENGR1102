ecec99cbb20470244919dba077bb90d6a7799453
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2018-01-17 09:18:37 -0500

    Added task 1 answer
    answer for task1

---
+++ tasks/1.operators/operators.py
@@ -0,0 +1,14 @@
+#Run python operators.py
+#Enter value to test
+
+x = int(input())
+
+def oddDoubleSquare(x):
+    if x%2==0:#enter solution here
+        print("even")
+    else:
+        print("odd")
+    print(x*2)
+    print(x**2)
+
+oddDoubleSquare(x)
