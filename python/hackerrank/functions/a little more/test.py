f83f4605198cb1213fd489d8940173c05bd00953
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2018-02-10 14:02:06 -0500

    finish functions test
    a little more

--- python/hackerrank/functions/a little more/test.py
+++ python/hackerrank/functions/a little more/test.py
@@ -4,11 +4,42 @@ try_zero = Rectangle(6,8)
 try_one = Rectangle(12,7)
 try_two = Rectangle(5,9)
 try_three = Rectangle(10,10)
-print(try_zero.getArea())
-print(try_zero.getPerimeter())
-print(try_one.getArea())
-print(try_one.getPerimeter())
-print(try_two.getArea())
-print(try_two.getPerimeter())
-print(try_three.getArea())
-print(try_three.getPerimeter())
+ansTryZero = [48,28]
+ansTryOne = [84,38]
+ansTryTwo = [45,28]
+ansTryThree = [100,40]
+
+print("Input: \n6\n8\n")
+print("Expected output: 48 28")
+print("User output:    ",try_zero.getArea(),try_zero.getPerimeter())
+if try_zero.getArea() != ansTryZero[0] and try_zero.getPerimeter() != ansTryZero[1]:
+    print("Testcase Failed!!!")
+else:
+    print("Testcase Passed!!!\n")
+
+print("Input: \n12\n7\n")
+print("Expected output: 84 38")
+print("User output:    ",try_one.getArea(),try_one.getPerimeter())
+if try_one.getArea() != ansTryOne[0] and try_one.getPerimeter() != ansTryOne[1]:
+    print("Testcase Failed!!!")
+else:
+    print("Testcase Passed!!!\n")
+
+print("Input: \n5\n9\n")
+print("Expected output: 45 28")
+print("User output:    ",try_two.getArea(),try_two.getPerimeter())
+if try_two.getArea() != ansTryTwo[0] and try_two.getPerimeter() != ansTryTwo[1]:
+    print("Testcase Failed!!!")
+else:
+    print("Testcase Passed!!!\n")
+    
+
+    
+print("Input: \n10\n10\n")
+print("Expected output: 100 40")
+print("User output:    ",try_three.getArea(),try_three.getPerimeter())
+if try_three.getArea() != ansTryThree[0] and try_three.getPerimeter() != ansTryThree[1]:
+    print("Testcase Failed!!!")
+else:
+    print("Testcase Passed!!!\n")
+
