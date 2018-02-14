d01921d855f5261bf89be7dea74c6b7fc3a20ef5
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2018-02-14 10:13:28 -0500

    in-n-out
    task completion

--- python/hackerrank/console input and outpu/in and out/test.py
+++ python/hackerrank/console input and outpu/in and out/test.py
@@ -1,2 +1,18 @@
 from task import in_n_out
+import pytest
+
+
+a = in_n_out()
+ansOne = "I Have 20 Total"
+if a == ansOne:
+    print("Test Case One Passed")
+else:
+    print("Test Case One Failed")
+
+ansTwo = "I Have 70 Guests!"
+b = in_n_out()
+if b == ansTwo:
+    print("Test Case Two Passed")
+else:
+    print("Test Case Two Failed")
 
