32ce34317c079424c494db79c7dda8d8a0fc0826
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2018-02-15 09:32:17 -0500

    update the atm
    better ux, need to do test

--- python/hackerrank/flow control/The ATM/task.py
+++ python/hackerrank/flow control/The ATM/task.py
@@ -15,7 +15,7 @@ def withdraw():
 
 def deposit():
     ###Assign the variable, aka the withraw print statement
-    string_variable = "You have selected to withdraw money from the account."
+    string_variable = "You have selected to deposit money to the account."
     
     ###Test Code Do Not Modify
     f = open("test.txt","a")
@@ -24,10 +24,26 @@ def deposit():
     
     #insert PRINT statement here 
     print("You have selected to deposit money to the account.")
+    
 def cancel():
+    ###Assign the variable, aka the withraw print statement
+    string_variable = "Returning to account selection."
+    
+    ###Test Code Do Not Modify
+    f = open("test.txt","a")
+    f.write(string_variable)
+    f.close()
+    
     #insert PRINT statement here 
     print("Returning to account selection.")
 def default():
+    ###Assign the variable, aka the withraw print statement
+    string_variable = "You have selected an invalid option."
+    
+    ###Test Code Do Not Modify
+    f = open("test.txt","a")
+    f.write(string_variable)
+    f.close()
+    
     #insert PRINT statement here 
-    print("You have selected an invalid option.")
-#Make sure you don't leave the quotation mark when changing below or it wont work.    
+    print("You have selected an invalid option.")  
