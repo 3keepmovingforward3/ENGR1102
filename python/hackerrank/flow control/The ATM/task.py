caf371333498a2190fab2d024a4a668be8d376fa
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2018-02-15 09:29:34 -0500

    the atm flow control
    works

---
+++ python/hackerrank/flow control/The ATM/task.py
@@ -0,0 +1,33 @@
+# Complete the function below.
+
+
+def withdraw():
+    ###Assign the variable, aka the withraw print statement
+    string_variable = "You have selected to withdraw money from the account."
+    
+    ###Test Code Do Not Modify
+    f = open("test.txt","a")
+    f.write(string_variable)
+    f.close()
+    
+    #insert PRINT statement here 
+    print("You have selected to withdraw money from the account.")
+
+def deposit():
+    ###Assign the variable, aka the withraw print statement
+    string_variable = "You have selected to withdraw money from the account."
+    
+    ###Test Code Do Not Modify
+    f = open("test.txt","a")
+    f.write(string_variable)
+    f.close()
+    
+    #insert PRINT statement here 
+    print("You have selected to deposit money to the account.")
+def cancel():
+    #insert PRINT statement here 
+    print("Returning to account selection.")
+def default():
+    #insert PRINT statement here 
+    print("You have selected an invalid option.")
+#Make sure you don't leave the quotation mark when changing below or it wont work.    
