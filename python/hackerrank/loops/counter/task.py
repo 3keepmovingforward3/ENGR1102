bc85fc5dc2103403c717d5fff7c721979d9c2605
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2018-02-15 09:08:23 -0500

    ux update counter
    update skeleton for user ux

--- python/hackerrank/loops/counter/task.py
+++ python/hackerrank/loops/counter/task.py
@@ -1,7 +1,11 @@
 def counter(endCount):
-    i=0
     f = open("test.txt","w")
+    ###User Code Below###
+    
+    i=0
     for i in range(endCount+1):
         f.write(str(i)+"\n")
         print(i)
+    
+    ###End User Code###
     f.close
