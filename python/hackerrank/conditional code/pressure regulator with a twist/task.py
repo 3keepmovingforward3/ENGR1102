cb56585d0a140e2581ae3e2bd687d819db6870c4
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2018-02-24 20:25:31 -0500

    pressure regulator file control
    made sha256 checksum of test.py
    zipped ans.py with rsa-256 p/w

--- python/hackerrank/conditional code/pressure regulator with a twist/task.py
+++ python/hackerrank/conditional code/pressure regulator with a twist/task.py
@@ -2,14 +2,4 @@
 
 def actuator(pressureWanted, pressureReading, decision):
     # Write your code here.
-    for i in range(len(pressureWanted)):
-        if pressureWanted[i] < 0 or pressureWanted[i] > 5:
-            pressureWanted[i] = 0
-    for i in range(len(pressureWanted)):
-        if pressureReading[i] < pressureWanted[i]:
-            decision[i] = 0
-        elif pressureReading[i] > pressureWanted[i]:
-            decision[i] = 1
-        else:
-            decision[i] = 2
-    return(decision)
+    return()
--- python/hackerrank/debugging/any comments/test.py
+++ python/hackerrank/debugging/any comments/test.py
@@ -6,3 +6,4 @@ while i < 10:
     if max(test) == findLargestNum(test):
         print("yes")
     i+=1
+
