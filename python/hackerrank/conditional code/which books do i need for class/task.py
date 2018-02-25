67a0b14a8520e029c808d6ca0479e0c52aaa1752
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2018-02-24 20:32:05 -0500

    which books file control
    zipped ans with p/w
    sha256 of test.py

--- python/hackerrank/conditional code/which books do i need for class/task.py
+++ python/hackerrank/conditional code/which books do i need for class/task.py
@@ -1,13 +1,9 @@
 def books_collections(book):
     #if the input string “book” equals “Calculus” return “I have my Calculus book.” 
-    if str(book) == "Calculus":
-        return("I have my Calculus book.")
+    
     #else if the input string “book” equals “History” return “I have my History book.” 
-    if str(book) == "History":
-        return("I have my History book.")
+    
     #else if the input string “book” equals “Circuits” return "I have my Circuits book." 
-    if str(book) == "Circuits":
-        return("I have my Circuits book.")
+    
     #for everything else return " I don't have that book." */
-    else:return("I don't have that book.")
-
+    
