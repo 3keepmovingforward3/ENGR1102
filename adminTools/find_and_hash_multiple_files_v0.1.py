739f691b40ea19da4113fd08bf087de73a5c9ccd
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2018-02-28 10:48:25 -0500

    add version and first comments

--- adminTools/find_and_hash_multiple_files_v0.1.py
+++ adminTools/find_and_hash_multiple_files_v0.1.py
@@ -1,5 +1,13 @@
 #! /usr/bin/env python3
 
+#Benjamin Blouin
+#2/28/2018
+#
+#This script can find a file by name, e.g. test.py, and also can take
+#the wildcard *, e.g. test.py*, and takes a flag -r, for recursive search
+#When a match is found, the file is hashed and the hash is saved in a new 
+#text file in the same folder
+
 import hashlib, fileinput, pathlib,os,glob,argparse
 from sys import argv
 
