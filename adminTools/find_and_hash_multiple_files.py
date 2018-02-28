81f89e9072fbedf7d7123222a826bab34c2a2ae4
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2018-02-28 10:33:47 -0500

    file_and_hash_multiple_files.py v0.1
    The script takes filename as argument, including a wildcard (works sometimes) and creates a file of the hash in the location of the file that's being hashed.  Has ability to be recursive, taking -r argument.
    
    The problem I have is when it encounters a folder that would be included with wildcard e.g.
    searching for: test* -r
    and a subfolder being /test...

---
+++ adminTools/find_and_hash_multiple_files.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python3
+
+import hashlib, fileinput, pathlib,os,glob,argparse
+from sys import argv
+
+
+def myParser():
+    parser = argparse.ArgumentParser(description='Hash some files')
+    parser.add_argument('filename', action="store", type=str)
+    parser.add_argument('-r', action='store_true',
+                        default=False,
+                        dest='flag_recursive',
+                        help='Set hashing to recursive')    
+    args = parser.parse_args()
+    if args.flag_recursive == True:
+        s = '**'
+    else:
+        s=''    
+    
+    return(parser.parse_args(),s)
+
+def process(line,hasher):
+    buf = line.encode('utf-16')
+    hasher.update(buf)
+    return((hasher.hexdigest()))
+
+args,s = myParser()
+hasher = hashlib.sha384()
+p = os.getcwd()
+
+for file in glob.glob('{}/{}/{}'.format(p,s,args.filename),recursive=args.flag_recursive):
+    for line in fileinput.input(file):
+        full_path = os.path.realpath(file)
+        folder_path_to_save_in = os.path.dirname(full_path)
+        l = os.path.basename(file)
+        theHash = process(line,hasher)
+        f = open('{}/{}-hash-sha384.txt'.format(folder_path_to_save_in,l),'w')
+        f.write(theHash)
+        print(full_path)
+        print(theHash)
+        f.close()
+        fileinput.nextfile()
+        fileinput.close()
