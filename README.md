ac35ccd4f0da89822cc39154f10d96bd1f46ed75
BenjaminBlouin <3keepmovingforward3@gmail.com>
2018-03-02 18:19:48 -0500

    Changing filename so as not to include 'hash'

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+# Temple University
+# Intro to Engineering - Programming
 # ENGR1102
+#
+# Repository Owned By Benjamin A. Blouin
+# benjamin.blouin@temple.edu
--- adminTools/find_and_hash_multiple_files_v0.1.py
+++ adminTools/find_and_hash_multiple_files_v0.1.py
@@ -1,18 +1,21 @@
-#! /usr/bin/env python3
+#! /usr/bin/env pypy3
 
-#Benjamin Blouin
-#2/28/2018
+# Benjamin Blouin
+# 2/28/2018
 #
-#This script can find a file by name, e.g. test.py, and also can take
-#the wildcard *, e.g. test.py*, and takes a flag -r, for recursive search
-#When a match is found, the file is hashed and the hash is saved in a new 
-#text file in the same folder
+# This script can find a file by name, e.g. test.py, and also can take
+# the wildcard *, e.g. test.py*, and takes a flag -r, for recursive search
+# When a match is found, the file is hashed and the hash is saved in a new
+# text file in the same folder
 
-import hashlib, fileinput, pathlib,os,glob,argparse
-from sys import argv
+import argparse
+import fileinput
+import glob
+import hashlib
+import os
 
 
-def myParser():
+def my_parser():
     parser = argparse.ArgumentParser(description='Hash some files')
     parser.add_argument('filename', action="store", type=str)
     parser.add_argument('-r', action='store_true',
@@ -20,29 +23,32 @@ def myParser():
                         dest='flag_recursive',
                         help='Set hashing to recursive')    
     args = parser.parse_args()
-    if args.flag_recursive == True:
+    if args.flag_recursive:
         s = '**'
     else:
-        s=''    
+        s = ''
     
-    return(parser.parse_args(),s)
+    return parser.parse_args(), s
 
-def process(line,hasher):
+
+def process(line, hasher):
     buf = line.encode('utf-16')
     hasher.update(buf)
-    return((hasher.hexdigest()))
+    return hasher.hexdigest()
 
-args,s = myParser()
+
+args, s = my_parser()
 hasher = hashlib.sha384()
 p = os.getcwd()
 
-for file in glob.glob('{}/{}/{}'.format(p,s,args.filename),recursive=args.flag_recursive):
+
+for file in glob.glob('{}/{}/{}'.format(p, s, args.filename), recursive=args.flag_recursive):
     for line in fileinput.input(file):
         full_path = os.path.realpath(file)
         folder_path_to_save_in = os.path.dirname(full_path)
         l = os.path.basename(file)
         theHash = process(line,hasher)
-        f = open('{}/{}-hash-sha384.txt'.format(folder_path_to_save_in,l),'w')
+        f = open('{}/{}-hash-sha384.txt'.format(folder_path_to_save_in, l), 'w')
         f.write(theHash)
         print(full_path)
         print(theHash)
