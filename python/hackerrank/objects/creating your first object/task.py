e4701c5f228abac07fb128b2db7957ec634ce53d
Benjamin Blouin <3keepmovingforward3@gmail.com>
2018-03-06 18:50:13 -0500

    creating your first object task fix

--- python/hackerrank/objects/creating your first object/task.py
+++ python/hackerrank/objects/creating your first object/task.py
@@ -1,24 +1,25 @@
 # This is the class definition in Python. 
 # This is how you define objects.
+
+
 class Rectangle(object):
     length = 0
     width = 0
-    
-    # This is an initializtion function for a class in Python.
+
+    # This is an initialization function for a class in Python.
     # 
     def __init__(self, length, width):
         # Fill in this function to define a new object
         # Hint: Each object has its own length and width.
         # Use the self keyword to initialize the length and width
-        self.length = length
-        self.width = width
-        
+        # Hint: self._____=_____
+
     # We are able to create class functions for use outside of the class.
     # This is normally to modify class variables.
     # Create two functions below to calculate the perimeter and area.
-    
+
     def getArea(self):
-        return(self.length*self.width)
+        return ()
+
     def getPerimeter(self):
-        return(2*(self.length+self.width))
-        
+        return ()
