d0f5d34c6d896486611525c526ce2e8d60d5c640
Benjamin Blouin <3keepmovingforward3@gmail.com>
2018-03-06 18:51:02 -0500

    iterating over objects task fix

--- python/hackerrank/objects/iterating over objects/task.py
+++ python/hackerrank/objects/iterating over objects/task.py
@@ -2,9 +2,5 @@
 
 def getCount(objects):
     # Write your code here.
-    x=0
-    z=len(objects)
-    for i in range(z):
-        if objects[i].x == objects[i].y:
-            x+=1
-    return(x)
+
+    return()
