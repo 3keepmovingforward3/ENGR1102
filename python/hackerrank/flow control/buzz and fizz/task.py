9cc38693b1d07156ce0961a0faa49ac644e14645
Benjamin Blouin <3keepmovingforward3@gmail.com>
2018-03-06 18:57:33 -0500

    fizzbuzz task fix

--- python/hackerrank/flow control/buzz and fizz/task.py
+++ python/hackerrank/flow control/buzz and fizz/task.py
@@ -1,14 +1,7 @@
 # Complete the function below.
 
+
 def fizzbuzz(fizzybubbly):
-    if (fizzybubbly % 3 == 0) & (fizzybubbly % 5 == 0):
-        return(str(fizzybubbly)+" fizzbuzz")
-    else:
-        if fizzybubbly % 5 == 0:
-            return(str(fizzybubbly)+" buzz")
-        else:
-            if fizzybubbly %3 == 0:
-                return(str(fizzybubbly)+" fizz")
-            else:
-                return(str(fizzybubbly)+ " This number is not divisible by 3 or 5")
-        
+    # User code here
+    # Use return statements
+    
