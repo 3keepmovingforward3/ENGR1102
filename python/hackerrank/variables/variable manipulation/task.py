d8d6649d88ed0988d700e80efbcc840bbb283f4f
Benjamin Blouin <3keepmovingforward3@gmail.com>
2018-03-06 18:44:05 -0500

    variable manipulation task fix

--- python/hackerrank/variables/variable manipulation/task.py
+++ python/hackerrank/variables/variable manipulation/task.py
@@ -1,26 +1,17 @@
-# This is a comment. Text formatted with /.../ is ignored when the code runs.
- # The next line of code that the computer will evaluate starts with the word "function"
- # A function is a user-defined command that you'll learn how to create in the future
- # The HackerRank system uses functions as its basic building block to give you feedback.
- # For now, you're just filling in the details.
- # Type your code where instructed below.
-
 def var_manipulation():
-    #Start your code below (tip: Make sure to indent your code)
-    var0 = 3.27
-    var1 = 18
-    var2 = True
-    var3 = 7
+    # Start your code below (tip: Make sure to indent your code)
+    var0 =
+    var1 =
+    var2 =
+    var3 =
     
-    ## Do not change the conditional statements below
+    # Do not change the conditional statements below
 
-    if (var0 == 3.27) or (var1 == 5):
-        if (var2 == True) & (var3 == 7):
+    if (var0 is 3.27) or (var1 is 5):
+        if (var2 is True) & (var3 is 7):
             result = var0 * var3
-            return("Here is the result! " + str(result))
-        else :
-            return("error in second if statement")
-    else :
-        return("error in first if statement")
-    
-print(var_manipulation())
+            return "Here is the result! {}".format(result)
+        else:
+            return "error in second if statement"
+    else:
+        return "error in first if statement"
--- python/hackerrank/variables/variable manipulation/test.py
+++ python/hackerrank/variables/variable manipulation/test.py
@@ -1,2 +1,5 @@
 from task import var_manipulation
 
+
+print(var_manipulation())
+assert var_manipulation() == 'Here is the result! 22.89'
