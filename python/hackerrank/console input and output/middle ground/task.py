68e764b8e5466e0d97659c482b611f6c05be2543
Benjamin Blouin <3keepmovingforward3@gmail.com>
2018-03-06 19:14:53 -0500

    middle ground task fix

--- python/hackerrank/console input and output/middle ground/task.py
+++ python/hackerrank/console input and output/middle ground/task.py
@@ -1,6 +1,3 @@
 def M_G():
-    #Start your code below (tip: Make sure to indent your code)
-    a = int(input())
-    b = int(input())
-    c = int(input())
-    return(a+b+c)
+    # Start your code below (tip: Make sure to indent your code)
+    return()
