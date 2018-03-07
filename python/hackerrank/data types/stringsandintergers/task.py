93d59b97307cc24d2a0bb878320edc18d98244cc
Benjamin Blouin <3keepmovingforward3@gmail.com>
2018-03-06 19:12:52 -0500

    strings and int task fix

--- python/hackerrank/data types/stringsandintergers/task.py
+++ python/hackerrank/data types/stringsandintergers/task.py
@@ -1,5 +1,4 @@
 
 def count_eggs(eggsJohn, eggsMary):
-    #Start your code below (tip: Make sure to indent your code)
-    total = eggsJohn + eggsMary
-    return("John and Mary have %d eggs total!" %total)
+    # Start your code below (tip: Make sure to indent your code)
+    return()
