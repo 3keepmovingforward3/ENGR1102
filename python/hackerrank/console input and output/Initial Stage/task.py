ab7586bf99f0d8e3093629b61723ebceee9f7503
Benjamin Blouin <3keepmovingforward3@gmail.com>
2018-03-09 12:05:08 -0500

    initial stage task answer removed + added task info as comments

--- python/hackerrank/console input and output/Initial Stage/task.py
+++ python/hackerrank/console input and output/Initial Stage/task.py
@@ -1,9 +1,21 @@
-def Init_Stage():
-    a = input('Input1: ')
-    b = input('Input2: ')
-    c = input('Input3: ')
-    return '{}{}\n{}'.format(a, b, c)
+# We're going to be working on input() user input functions.
+
+# Implement a function named init_stage that has no parameter inputs but must read
+# in inputs by utilizing the user input function in Python
+
+# Sample input test case 1: 5 15 30
+# Output: 515
+#         30
+
+# Sample input test case 1: 5 16 57
+# Output: 516
+#         57
 
 
-a = Init_Stage()
+def init_stage():
+
+    return
+
+
+a = init_stage()
 print(a)
