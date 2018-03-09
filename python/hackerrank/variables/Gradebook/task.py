dec9ca3483ce628f83fa2f676e7dcbf71b55f772
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2018-03-08 21:35:42 -0500

    Variable creating added task instructions as comments

--- python/hackerrank/variables/Gradebook/task.py
+++ python/hackerrank/variables/Gradebook/task.py
@@ -1,5 +1,14 @@
+# Task 
+# Declare a variable and set it as "Penguin".
+# Declare a second variable and set it as the numerical value # # 6.
+# Declare a third variable and set it as Boolean False.
+# Declare a fourth variable but give it no value.
+# Declare a fifth variable and assign it a float value
+# Print the 5 variables separately to the console.
+
+
+
 def grade_avrg():
     
     # User Code Below
     return ()
-
