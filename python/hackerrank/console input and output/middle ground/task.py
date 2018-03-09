94151143576b5203d32f1be820dd692e4a865b30
Benjamin Blouin <3keepmovingforward3@gmail.com>
2018-03-09 12:14:59 -0500

    middle ground pep8 fix

--- python/hackerrank/console input and output/middle ground/task.py
+++ python/hackerrank/console input and output/middle ground/task.py
@@ -1,6 +1,7 @@
 # Function takes three integers as input, sums them, and returns the answer
 # Sample input: 5 16 57 Output: 78
 
+
 def m_g():
     # Start your code below (tip: Make sure to indent your code)
     return()
