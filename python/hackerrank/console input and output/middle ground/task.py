57d5aeed634eaff8baf0fc082619a3b08b2ceb2e
Benjamin Blouin <3keepmovingforward3@gmail.com>
2018-03-09 12:14:04 -0500

    middle ground task answer removed + added task info as comments

--- python/hackerrank/console input and output/middle ground/task.py
+++ python/hackerrank/console input and output/middle ground/task.py
@@ -1,3 +1,6 @@
-def M_G():
+# Function takes three integers as input, sums them, and returns the answer
+# Sample input: 5 16 57 Output: 78
+
+def m_g():
     # Start your code below (tip: Make sure to indent your code)
     return()
